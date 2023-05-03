df10b4ad0bd8417f78eaa802d9737f4f05dde39c
ser356 <70541693+ser356@users.noreply.github.com>
2023-05-04 01:47:13 +0200

    Update optimizado.s

--- optimizado.s
+++ optimizado.s
@@ -16,25 +16,23 @@ mediaV: .float 0.0
 .text
 .global main
 main:
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
                                                     ; Calculo de los primeros 10 elementos de la secuencia
     lf      f0,             valor_inicial           ; f0=5.0
     lw      r8,             tamanho                 ; r8=30 pero se puede utilizar cualquier valor en rango de 10 a 30
-    addi    r9,             r0,             0       ; inicializamos el puntero de posicion del vector
-    sf      vector(r9),     f1                      ; primera posicion del vector a 0 puesto que el primer valor d ela secuencia es 0
-    addi    r9,             r9,             4       ; incrementamos el puntero del vector en 4 bytes puesto que es un float
-    sf      vector(r9),     f0                      ; segunda posicion del vector a 5 ya que es el valor inicial, segundo elemento en la secuencia
-    addi    r10,            r0,             5       ; inicializamos el contador de tamanho de la secuencia
-    addi    r9,             r9,             4       ; incrementamos el puntero del vector en 4 bytes puesto que es un float
-    addf    f18,            f0,             f1      ; calculamos el valor de la secuencia en n
-    sf      vector(r9),     f18                     ; almacenamos en memoria el valor de la secuencia en vector[n]
-    addi    r9,             r9,             4       ; incrementamos el puntero del vector en 4 bytes puesto que es un float
-    addf    f19,            f18,            f0      ; calculamos el valor de la secuencia en n
-    sf      vector(r9),     f19                     ; almacenamos en memoria el valor de la secuencia en vector[n]
-    addi    r9,             r9,             4       ; incrementamos el puntero del vector en 4 bytes puesto que es un float
-    addf    f20,            f19,            f18     ; calculamos el valor de la secuencia en n
-    sf      vector(r9),     f20                     ; almacenamos en memoria el valor de la secuencia en vector[n]
 
+    addi    r9,             r0,             20      ; incrementamos el puntero del vector
+    sf      vector-16(r9),  f0                      ; almacenamos en memoria el valor de la secuencia en vector[n]
+    sf      vector-12(r9),  f0                      ; segunda posicion del vector a 5 ya que es el valor inicial, segundo elemento en la secuencia
+    addf    f18,            f0,             f0      ; calculamos el valor de la secuencia en n
+
+    sf      vector-8(r9),   f18                     ; almacenamos en memoria el valor de la secuencia en vector[n]
+    addf    f19,            f18,            f0      ; calculamos el valor de la secuencia en n
+
+    sf      vector-4(r9),   f19                     ; almacenamos en memoria el valor de la secuencia en vector[n]
+    addf    f20,            f19,            f18     ; calculamos el valor de la secuencia en n
+
+    sf      vector(r9),     f20                     ; almacenamos en memoria el valor de la secuencia en vector[n]
 
 
     addi    r9,             r9,             20      ; incrementamos el puntero del vector
@@ -66,10 +64,10 @@ main:
 
     sf      vector(r9),     f17                     ; almacenamos en memoria
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
     jal     opMat                                   ; SALTO INCONDICIONAL PARA CALCULAR MATRIZ ASOCIADA A LA SECUENCIA
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
     addi    r11,            r10,            10
     seqi    r11,            r8,             10
     bnez    r11,            fin
@@ -104,12 +102,12 @@ main:
 
     sf      vector(r9),     f17                     ; almacenamos en memoria
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
     ;SE     HAN,            CALCULADO,      OTROS,  CINCO,VALORES,DE,LA,SECUENCIA; EL ENUNCIADO PIDE MULTIPLOS DE 5
 
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
     addi    r11,            r10,            10
     seq     r11,            r8,             15
@@ -141,11 +139,11 @@ main:
 
     sf      vector(r9),     f17                     ; almacenamos en memoria
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
 ; OTROS 5 VALORES DE LA SECUENCIA HAN SIDO CALCULADOS
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
     addi    r11,            r10,            10
     seq     r11,            r8,             20
@@ -180,10 +178,10 @@ main:
 
     sf      vector(r9),     f17                     ; almacenamos en memoria
 
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
 ; OTROS 5 VALORES DE LA SECUENCIA HAN SIDO CALCULADOS
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;
 
     addi    r11,            r10,            10
     seq     r11,            r8,             25
@@ -234,13 +232,13 @@ main:
 
 fin:
 
-    ; valor31=f17+f14
-    ; valor32=f14+valor31
-    addf f28  , f17, f14
-    addf f29  , f28,f17
-    subf f30  , f29,f0 
-    sf   suma, f30
-    
+; valor31=f17+f14
+; valor32=f14+valor31
+    addf    f28,            f17,            f14
+    addf    f29,            f28,            f17
+    subf    f30,            f29,            f0
+    sf      suma,           f30
+
     trap    0
 
 
