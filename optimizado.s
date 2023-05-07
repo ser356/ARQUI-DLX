184068c33556f54d0dd5b0623dee93933f9e2c1a
ser356 <70541693+ser356@users.noreply.github.com>
2023-05-08 00:15:19 +0200

    reviewed+comented+reduced

--- optimizado.s
+++ optimizado.s
@@ -23,21 +23,26 @@ main:
     lf      f0,             valor_inicial
     lw      r8,             tamanho
 
-    addi    r9,             r0,             120
+    addi    r9,             r0,             120     ; puntero al vector 120=30*4 podemos desplazarnos por todo el vector de un salto
 
-
+; los dos stores seguidos se reordenan para aprovechar el tiempo de calculo de la multiplicacion
+; calculo de la secuencia hasta 10 terminos
+; se adelantan hasta 3 (10+3)valores de la siguiente secuencia para que el calculo sea optimizado
+; ademas los valores 11 y 12 sirven para calcular la media de la secuencia
     addf    f18,            f0,             f0
-
-    sf      vector-108(r9), f18
     addf    f19,            f18,            f0
 
+    sf      vector-108(r9), f18
+
     sf      vector-104(r9), f19
+
+
     addf    f20,            f19,            f18
 
+    addf    f4,             f20,            f19
 
     sf      vector-100(r9), f20
 
-    addf    f4,             f20,            f19
     sf      vector-96(r9),  f4
 
 
@@ -51,10 +56,10 @@ main:
     sf      M+8,            f8
 
     multf   f10,            f20,            f11
+    sf      vector-84(r9),  f14
 
 
     addf    f14,            f11,            f8
-    sf      vector-84(r9),  f14
 
 
 
@@ -80,17 +85,19 @@ main:
 
     addf    f17,            f14,            f11
     multf   f16,            f8,             f4
+    sf      V,              f7
+
     addf    f15,            f17,            f14
     subf    f27,            f17,            f4
 
     divf    f28,            f27,            f1
     sf      mediaM,         f28
     sf      M+4,            f4
-    sf      V,              f7
     multf   f29,            f28,            f8
+    sf      V+4,            f16
+
     addf    f30,            f16,            f7
     sf      M+12,           f11
-    sf      V+4,            f16
 
     addf    f0,             f16,            f30
     sf      V+8,            f30
@@ -98,9 +105,9 @@ main:
 
     sf      mediaV,         f29
     addf    f8,             f7,             f6
+    addf    f4,             f15,            f17
 
-                                                    ; descomentar para valorsecuencia>=10
-
+; salto condicional para valores de la secuencia hasta 10
 
     seqi    r11,            r8,             10
     bnez    r11,            fin
@@ -117,8 +124,8 @@ main:
     sf      vector-76(r9),  f15
 
 
-    addf    f4,             f15,            f17
     sf      vector-72(r9),  f4
+                                                    ; solo se calculan dos porque se han adelantado tres anteriormente
 
     addf    f5,             f4,             f15
     sf      vector-68(r9),  f5
@@ -127,18 +134,22 @@ main:
     sf      vector-64(r9),  f6
 
 
+; se adelantan 3 de la siguiente
+    addf    f7,             f6,             f5
 
+    addf    f8,             f7,             f6
+
+    addf    f9,             f8,             f7
+
+; salto condicional para valores de la secuencia hasta 15
     seqi    r11,            r8,             15
     bnez    r11,            fin
 
 
-    addf    f7,             f6,             f5
     sf      vector-60(r9),  f7
 
-    addf    f8,             f7,             f6
     sf      vector-56(r9),  f8
 
-    addf    f9,             f8,             f7
     sf      vector-52(r9),  f9
 
     addf    f10,            f9,             f8
@@ -147,18 +158,21 @@ main:
     addf    f11,            f10,            f9
     sf      vector-44(r9),  f11
 
-
+    addf    f12,            f11,            f10
+    addf    f13,            f12,            f11
+    addf    f14,            f13,            f12
+                                                    ; se pueden adelantar hasta 3 valores de la secuencia para que el calculo sea optimizado
     seqi    r11,            r8,             20
     bnez    r11,            fin
 
 
-    addf    f12,            f11,            f10
+
     sf      vector-40(r9),  f12
 
-    addf    f13,            f12,            f11
+
     sf      vector-36(r9),  f13
 
-    addf    f14,            f13,            f12
+
     sf      vector-32(r9),  f14
 
     addf    f15,            f14,            f13
@@ -166,19 +180,20 @@ main:
 
     addf    f16,            f15,            f14
     sf      vector-24(r9),  f16
-
-
+    addf    f17,            f16,            f15
+    addf    f18,            f17,            f16
+    addf    f19,            f18,            f17
     seqi    r11,            r8,             25
     bnez    r11,            fin
 
 
-    addf    f17,            f16,            f15
+
     sf      vector-20(r9),  f17
 
-    addf    f18,            f17,            f16
+
     sf      vector-16(r9),  f18
 
-    addf    f19,            f18,            f17
+
     sf      vector-12(r9),  f19
 
     addf    f20,            f19,            f18
